dcafc8535bc0c84d11b5c0139fd284d64bd15aaa
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-26 15:35:19 +0500

    Add the InitDB function to connect to the todos.db database

---
+++ internals/db/sqlite.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/glebarez/go-sqlite"
+)
+
+func InitDB(filepath string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	// Инициализация схемы
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS todos (
+            id TEXT PRIMARY KEY,
+            title TEXT NOT NULL,
+            completed INTEGER NOT NULL,
+			created_at TEXT NOT NULL
+        )
+    `)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
