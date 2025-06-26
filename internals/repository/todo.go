ae0e52e2277e520650c0291246162fc967fefcbd
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-26 17:19:26 +0500

    Update ToDoRepository to include GetByID, UpdateTitle, UpdateCompleted and Delete methods

--- internals/repository/todo.go
+++ internals/repository/todo.go
@@ -2,19 +2,19 @@ package repository
 
 import (
 	"database/sql"
-	_ "database/sql"
-	_ "fmt"
 	"log"
-	_ "os"
 	"sync"
-	_ "sync"
 
 	models "myproject/internals/models"
 )
 
 type ToDoRepository interface {
 	GetAll() ([]*models.ToDo, error)
+	GetByID(id string) (*models.ToDo, error)
 	Create(todo *models.ToDo) error
+	UpdateTitle(id string, title string) error
+	UpdateCompleted(id string, completed bool) error
+	Delete(id string) error
 }
 
 type SQLiteToDoRepository struct {
@@ -24,6 +24,22 @@ type SQLiteToDoRepository struct {
 
 func NewSQLiteToDoRepository(db *sql.DB) *SQLiteToDoRepository {
 	return &SQLiteToDoRepository{db: db}
+}
+
+func (r *SQLiteToDoRepository) Create(todo *models.ToDo) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	result, err := r.db.Exec("INSERT INTO todos (id, title, completed, created_at) VALUES (?, ?, ?, ?)", todo.ID, todo.Title, todo.Completed, todo.CreatedAt)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	todo.ID = string(id)
+	return nil
 }
 
 func (r *SQLiteToDoRepository) GetAll() ([]*models.ToDo, error) {
@@ -47,18 +63,52 @@ func (r *SQLiteToDoRepository) GetAll() ([]*models.ToDo, error) {
 	return todos, nil
 }
 
-func (r *SQLiteToDoRepository) Create(todo *models.ToDo) error {
+func (r *SQLiteToDoRepository) GetByID(id string) (*models.ToDo, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	row, err := r.db.Query("SELECT * FROM todos WHERE id=?", id)
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+
+	todo := models.ToDo{}
+	if err := row.Scan(&todo.ID, &todo.Title, &todo.Completed, &todo.CreatedAt); err != nil {
+		return nil, err
+	}
+	return &todo, nil
+}
+
+func (r *SQLiteToDoRepository) UpdateTitle(id string, title string) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	result, err := r.db.Exec("INSERT INTO todos (id, title, completed, created_at) VALUES (?, ?, ?, ?)", todo.ID, todo.Title, todo.Completed, todo.CreatedAt)
+	_, err := r.db.Exec("UPDATE todos SET title = ? WHERE id = ?", title, id)
 	if err != nil {
 		return err
 	}
-	id, err := result.LastInsertId()
+	return nil
+}
+
+func (r *SQLiteToDoRepository) UpdateCompleted(id string, completed bool) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, err := r.db.Exec("UPDATE todos SET completed = ? WHERE id = ?", completed, id)
 	if err != nil {
 		return err
 	}
-	todo.ID = string(id)
+	return nil
+}
+
+func (r *SQLiteToDoRepository) Delete(id string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, err := r.db.Exec("DELETE FROM todos WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
 	return nil
 }
