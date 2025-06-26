97b169585353bb3962c37dea99c990a435084a69
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-26 15:51:51 +0500

    Add ToDoService for handling task business logic

---
+++ internals/services/todo.go
@@ -0,0 +1,46 @@
+package services
+
+import (
+	"errors"
+	"myproject/internals/db"
+	"myproject/internals/models"
+	"myproject/internals/repository"
+	"time"
+
+	"github.com/google/uuid"
+	_ "github.com/google/uuid"
+)
+
+type ToDoService struct {
+	repo repository.ToDoRepository
+}
+
+func NewToDoService() (*ToDoService, error) {
+	db_, err := db.InitDB("./todos.db")
+	if err != nil {
+		return nil, err
+	}
+	repo := repository.NewSQLiteToDoRepository(db_)
+
+	return &ToDoService{repo: repo}, nil
+}
+
+func (s *ToDoService) GetAll() ([]*models.ToDo, error) {
+	return s.repo.GetAll()
+}
+
+func (s *ToDoService) Create(title string) (*models.ToDo, error) {
+	if title == "" {
+		return nil, errors.New("title cannot be empty")
+	}
+	todo := &models.ToDo{
+		ID:        uuid.NewString(),
+		Title:     title,
+		Completed: false,
+		CreatedAt: time.Now(),
+	}
+	if err := s.repo.Create(todo); err != nil {
+		return nil, err
+	}
+	return todo, nil
+}
