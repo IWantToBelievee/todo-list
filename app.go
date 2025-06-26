cd9b94ba2ea72174bef4d521d704e8e7b52facf4
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-26 16:41:08 +0500

    Add ToDoService to App

--- app.go
+++ app.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"context"
+	"myproject/internals/models"
+	"myproject/internals/services"
 )
 
 type App struct {
-	ctx context.Context
+	ctx     context.Context
+	todoSvc *services.ToDoService
 }
 
 func NewApp() *App {
-	return &App{}
+	todoSvc, err := services.NewToDoService()
+	if err != nil {
+		return &App{todoSvc: nil}
+	}
+	return &App{todoSvc: todoSvc}
 }
 
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func (a *App) shutdown(ctx context.Context) {
+func (a *App) shutdown(ctx context.Context) {}
+
+func (a *App) GetToDos() ([]*models.ToDo, error) {
+	return a.todoSvc.GetAll()
+}
+
+func (a *App) CreateToDo(title string) (*models.ToDo, error) {
+	return a.todoSvc.Create(title)
 }
