0f49744a39e8527da2e6cacae2da88950c6b75e9
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-27 14:09:34 +0500

    Update ToDo model by changing names of fileds

--- internals/models/todo.go
+++ internals/models/todo.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type ToDo struct {
-	ID        string
-	Title     string
-	Completed bool
-	CreatedAt time.Time
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
 }
