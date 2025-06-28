7a705f1fd2d2bc8ae2729cfc4ffec45d04417bc2
IWantToBelievee <artemkursaevkz@gmail.com>
2025-06-28 19:24:09 +0500

    Add ToDoList to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import ToDoList from './ToDoList';
 
 function App() {
 
     return (
         <div id="App">
-            
+            <ToDoList/>
         </div>
     )
 }
