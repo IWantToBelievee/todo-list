affabccb8f9333b3692297895ba62a5d8dcbc4a2
IWantToBelievee <artemkursaevkz@gmail.com>
2025-07-05 18:45:07 +0500

    Update ToDoList by adding drag-and-drop function to sorting todos

--- frontend/src/ToDoList.tsx
+++ frontend/src/ToDoList.tsx
@@ -1,25 +1,43 @@
 import './App.css'
-import { DeleteToDo } from '../wailsjs/go/main/App';
-import ToDoItem from "./ToDoItem";
-import ToDo from './types';
+import { DeleteToDo, SaveOrder } from '../wailsjs/go/main/App';
+import SortableItem from './SortableItem';
+import { DndContext } from '@dnd-kit/core';
+import { arrayMove, SortableContext } from '@dnd-kit/sortable';
+import {
+  restrictToWindowEdges,
+  createSnapModifier,
+} from '@dnd-kit/modifiers'
 
-interface ToDoListProps {
-    todos: ToDo[];
-    loadTodos: Function;
-}
-
-export default function ToDoList({ todos, loadTodos }: ToDoListProps) {
+export default function ToDoList({ todos, loadTodos, updateTodo, toggleCompleted, order, setOrder, loadOrder }) {
     const deleteTodo = async (id: string) => {
         await DeleteToDo(id);
         loadTodos();
+        loadOrder();
     };
 
+    const onDragEnd = (e) => {
+        const {active, over} = e;
+        if (over && active.id !== over.id) {
+            setOrder((items) => {
+                const oldIndex = items.findIndex((item) => item === active.id);
+                const newIndex = items.findIndex((item) => item === over.id);
+                const newOrder = arrayMove(items, oldIndex, newIndex);
+                SaveOrder(newOrder);
+                return newOrder;
+            });
+        }
+    }
+
     return (
-        <div className="flex-12 w-full ml-25 transition-all delay-75">
+        <div className="flex-12 w-full ml-25">
             <div className='flex flex-row gap-4 flex-wrap pt-4'>
-                {todos && todos.map((todo) => (
-                    <ToDoItem todo={todo} deleteTodo={deleteTodo} />
-                ))}
+                <DndContext onDragEnd={onDragEnd} modifiers={[restrictToWindowEdges]}>
+                    <SortableContext items={order}>
+                        {order && order.map((item) => (
+                            <SortableItem key={item} id={item} todo={todos.find(i => i.id === item)} deleteTodo={deleteTodo} updateTodo={updateTodo} toggleCompleted={toggleCompleted} />
+                        ))}
+                    </SortableContext>
+                </DndContext>
             </div>
         </div>
     );
